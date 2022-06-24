c781d3312f4d23652ff5efb9921bbe2badbd5cd8
Pablo Munoz Lara <pabloml@outlook.es>
2022-06-25 00:52:12 +0200

    Added empty content view and loading for detail

--- appmvvm/src/main/java/com/slyrand/mvvmapp/user/detail/UserDetailFragment.kt
+++ appmvvm/src/main/java/com/slyrand/mvvmapp/user/detail/UserDetailFragment.kt
@@ -6,8 +6,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.slyrand.domain.core.model.DataError
+import com.slyrand.domain.user.model.User
 import com.slyrand.mvvmapp.R
 import com.slyrand.mvvmapp.core.extensions.loadThumbnail
 import com.slyrand.mvvmapp.databinding.FragmentUserDetailBinding
@@ -32,24 +33,53 @@ class UserDetailFragment : Fragment(R.layout.fragment_user_detail) {
             toolBar.setNavigationOnClickListener { findNavController().navigateUp() }
             viewLifecycleOwner.lifecycleScope.launch {
                 viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    _viewModel.state.collect { state ->
-
-                        state.user?.let { user ->
-                            userThumbnail.loadThumbnail(state.user.picture)
-                            userName.text = "${user.firstName} ${user.lastName}"
-                            userEmail.setText(user.email)
-                            userPhone.setText(user.phone)
-
-                            val dateFormatter = SimpleDateFormat(
-                                "dd / MM / yyyy",
-                                Locale.getDefault()
-                            )
-                            user.dateOfBirth?.let {
-                                userBirthDate.setText(dateFormatter.format(it))
-                            }
-                        }
-                    }
+                    _viewModel.state.collect { state -> updateUi(state) }
                 }
+            }
+        }
+    }
+
+    private fun updateUi(state: UsersDetailViewModel.UiState) {
+        with(_binding) {
+            if (state.error == null) emptyContentView.visibility = View.GONE
+            else {
+                emptyContentView.visibility = View.VISIBLE
+                updateError(state.error)
+            }
+
+            progress.visibility = if (state.loading) View.VISIBLE else View.GONE
+            state.user?.let { user -> updateUserInfo(user) }
+        }
+    }
+
+    private fun updateError(error: DataError) {
+        with(_binding) {
+            when (error) {
+                is DataError.ConnectionError -> {
+                    emptyContentView.setTitle(getString(R.string.connection_error_title))
+                    emptyContentView.setSubtitle(getString(R.string.connection_error_subtitle))
+                }
+                else -> {
+                    emptyContentView.setTitle(getString(R.string.generic_error_title))
+                    emptyContentView.setSubtitle(getString(R.string.generic_error_subtitle))
+                }
+            }
+        }
+    }
+
+    private fun updateUserInfo(user: User) {
+        with(_binding) {
+            userThumbnail.loadThumbnail(user.picture)
+            userName.text = "${user.firstName} ${user.lastName}"
+            userEmail.setText(user.email)
+            userPhone.setText(user.phone)
+
+            val dateFormatter = SimpleDateFormat(
+                "dd / MM / yyyy",
+                Locale.getDefault()
+            )
+            user.dateOfBirth?.let {
+                userBirthDate.setText(dateFormatter.format(it))
             }
         }
     }
